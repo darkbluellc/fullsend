14bd2a81c4eaf78addbe9f232d13d0eadd28600b
Dan Bruce <dan@ddbruce.com>
2022-07-20 09:08:45 -0400

    working twilio messages

--- src/groups.js
+++ src/groups.js
@@ -1,9 +1,16 @@
 const { execQuery } = require("./db");
 
 const CONTACTS_IN_GROUP_GET =
-  "SELECT * FROM `contacts` WHERE id in (SELECT contact_id FROM contacts_groups where group_id = ?)";
+  "SELECT * FROM contacts WHERE id IN (SELECT contact_id FROM contacts_groups WHERE group_id = ?)";
+const NUMBERS_IN_GROUP_GET =
+  "SELECT phone_number FROM contacts WHERE id IN (SELECT contact_id FROM contacts_groups WHERE group_id = ?)";
 
 exports.getContactsInGroup = (pool, group) =>
   execQuery(pool, CONTACTS_IN_GROUP_GET, group, (results) => {
+    return results;
+  });
+
+exports.getNumbersinGroup = (pool, group) =>
+  execQuery(pool, NUMBERS_IN_GROUP_GET, group, (results) => {
     return results;
   });
--- src/messages.js
+++ src/messages.js
@@ -1,19 +1,46 @@
 const { execQuery } = require("./db");
+const twilio = require("twilio");
+
+const groupsApi = require("./groups.js");
+
+require("dotenv").config();
+
+const TWILIO_FROM = process.env.TWILIO_FROM;
+
+const client = new twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_TOKEN
+);
 
 const MESSAGE_ADD =
   "INSERT INTO messages (`id`, `user_id`, `text`, `sent_at`) VALUES (null, ?, ?, NOW())";
 const MESSAGE_GROUP_ADD = "INSERT INTO messages_groups VALUES (?, ?)";
 
 exports.sendMessage = (pool, userId, text, groups) => {
+  let numbers = [];
   execQuery(pool, MESSAGE_ADD, [userId, text], async (results) => {
     if (results) {
       for (const group of groups) {
+        const groupResponse = await groupsApi.getNumbersinGroup(pool, group);
+        for (const groupNumber of groupResponse.data) {
+          const number = groupNumber.phone_number;
+          numbers.push(number);
+        }
         execQuery(
           pool,
           MESSAGE_GROUP_ADD,
           [Number(results.insertId), group],
           async (results) => {}
         );
+      }
+      for (const number of numbers) {
+        client.messages
+          .create({
+            body: text,
+            to: number,
+            from: TWILIO_FROM,
+          })
+          .then((message) => console.info(message));
       }
     }
   });
