84e8ddc8ccad5e928e3bfd34d6f13f36a92b4066
Dan Bruce <dan@ddbruce.com>
2022-07-23 20:37:10 -0400

    add logged in/out check to help page

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,9 @@
 window.onload = () => {
   const session = getCookie("fullsend_session");
-  console.log(session);
 
   if (session) {
     (async () => {
       const sessionInfo = (await checkLogin()).data;
-      console.log((await sessionInfo).length);
       if ((await sessionInfo).length != 0) window.location.href = "/fullsend";
     })();
   }
