f5c39116209e138888f3ef00c509391b85a45a92
Dan Bruce <dan@ddbruce.com>
2022-07-23 20:30:03 -0400

    make home.js immediately invoked function

--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,12 @@
 window.onload = () => {
   const session = getCookie("fullsend_session");
+  console.log(session);
 
   if (session) {
-    async () => {
-      const sessionInfo = (await checkLogin(session)).data;
+    (async () => {
+      const sessionInfo = (await checkLogin()).data;
+      console.log((await sessionInfo).length);
       if ((await sessionInfo).length != 0) window.location.href = "/fullsend";
-    };
+    })();
   }
 };
