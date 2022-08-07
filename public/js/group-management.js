d2e432920541af433ba96f28048e18e007733537
Dan Bruce <dan@ddbruce.com>
2022-08-07 13:34:26 -0400

    working group management (recipients only for now)

--- public/js/group-management.js
+++ public/js/group-management.js
@@ -1,5 +1,5 @@
-let contactsLoaded = false;
 let recipientListReady = false;
+let currentGroup;
 
 const getGroups = async () => {
   const session = getCookie("fullsend_session");
@@ -29,7 +29,7 @@ const loadContacts = async () => {
   for (const contact of contacts) {
     document.getElementById(
       "groupManagementRecipients"
-    ).innerHTML += `<input class="form-check-input recipient-switch" type="checkbox" role="switch" value=${contact.id} id="recipient-switch-${contact.id}">
+    ).innerHTML += `<input class="form-check-input recipient-switch" type="checkbox" role="switch" value=${contact.id} id="recipient-switch-${contact.id}" disabled onchange="handleSwitch(event);">
       <label class="form-check-label" for="recipient-switch-${contact.id}">${contact.first_name} ${contact.last_name}</label><br>
       `;
   }
@@ -50,6 +50,7 @@ const setGroupContacts = async (groupId) => {
 
   const checkboxes = document.getElementsByClassName("recipient-switch");
   for (const checkbox of checkboxes) {
+    checkbox.disabled = false;
     checkbox.checked = false;
   }
 
@@ -58,6 +59,36 @@ const setGroupContacts = async (groupId) => {
   }
 
   recipientListReady = true;
+};
+
+const handleSwitch = async (e) => {
+  const session = getCookie("fullsend_session");
+
+  if (!recipientListReady) return;
+  const userId = e.target.value;
+  const action = e.target.checked ? "add" : "remove";
+
+  if (action == "add") {
+    const result = await fetch("/auth/api/groups/update/addcontact", {
+      method: "POST",
+      headers: { "Content-Type": "application/json", session: session },
+      body: JSON.stringify({
+        contactId: userId,
+        groupId: currentGroup,
+      }),
+    });
+    return result;
+  } else if (action == "remove") {
+    const result = await fetch("/auth/api/groups/update/removecontact", {
+      method: "POST",
+      headers: { "Content-Type": "application/json", session: session },
+      body: JSON.stringify({
+        contactId: userId,
+        groupId: currentGroup,
+      }),
+    });
+    return result;
+  }
 };
 
 const pageOnLoadFunctions = async () => {
@@ -70,14 +101,11 @@ const pageOnLoadFunctions = async () => {
     groupManagementSelect.innerHTML += `<option value="${group.id}">${group.name}</option>`;
   }
 
+  loadContacts();
+
   groupManagementSelect.onchange = () => {
     const groupId = groupManagementSelect.value;
-
-    if (!contactsLoaded) {
-      loadContacts();
-      contactsLoaded = true;
-    }
-
     setGroupContacts(groupId);
+    currentGroup = groupId;
   };
 };
