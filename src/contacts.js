3c040d445ce7ca32847ee8d5d13839a3d50e4d37
Dan Bruce <dan@ddbruce.com>
2023-01-11 14:50:18 -0500

    only show active users on group mgmt page

--- src/contacts.js
+++ src/contacts.js
@@ -2,4 +2,9 @@ const { execQuery } = require("./db");
 
 const CONTACTS_GET = "SELECT * FROM contacts ORDER BY last_name";
 
+const CONTACTS_ACTIVE_GET =
+  "SELECT * FROM contacts ORDER BY last_name WHERE active = 1";
+
 exports.getContacts = (pool) => execQuery(pool, CONTACTS_GET, null);
+exports.getActiveContacts = (pool) =>
+  execQuery(pool, CONTACTS_ACTIVE_GET, null);
