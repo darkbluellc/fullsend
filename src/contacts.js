a9fc373778c0fcfd527dffbc216d66c20adac533
Dan Bruce <dan@ddbruce.com>
2023-01-11 14:58:20 -0500

    fix last pr

--- src/contacts.js
+++ src/contacts.js
@@ -3,7 +3,7 @@ const { execQuery } = require("./db");
 const CONTACTS_GET = "SELECT * FROM contacts ORDER BY last_name";
 
 const CONTACTS_ACTIVE_GET =
-  "SELECT * FROM contacts WHERE active = 1 ORDER BY last_name";
+  "SELECT * FROM contacts WHERE enabled = 1 ORDER BY last_name";
 
 exports.getContacts = (pool) => execQuery(pool, CONTACTS_GET, null);
 exports.getActiveContacts = (pool) =>
