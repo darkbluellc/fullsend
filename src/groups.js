61bd2dd46a9c8bdb959613c71cf67d906916b76e
Dan Bruce <ddbruce@users.noreply.github.com>
2023-02-27 19:50:30 -0500

    Fix enabled users issue (#30)
    * fix issue with disabled users still being selected
    
    * update changelog
    
    * 1.5.1

--- src/groups.js
+++ src/groups.js
@@ -4,11 +4,12 @@ const GROUPS_GET = "SELECT * FROM groups";
 const GROUPS_SEQ_ACTIVE_GET =
   "SELECT * FROM groups WHERE active = 1 ORDER BY sequence";
 const CONTACTS_IN_GROUP_GET =
-  "SELECT * FROM contacts WHERE id IN (SELECT contact_id FROM contacts_groups WHERE group_id = ?)";
+  "SELECT * FROM contacts WHERE id IN (SELECT contact_id FROM contacts_groups WHERE group_id = ? AND enabled = 1) AND enabled = 1";
 
 const CONTACTS_IN_MULTIPLE_GROUPS_GET_A =
   "SELECT * FROM contacts WHERE id IN (SELECT contact_id FROM contacts_groups WHERE group_id IN (";
-const CONTACTS_IN_MULTIPLE_GROUPS_GET_B = ")) ORDER BY last_name";
+const CONTACTS_IN_MULTIPLE_GROUPS_GET_B =
+  ") AND enabled = 1) AND enabled = 1 ORDER BY last_name";
 
 const NUMBERS_IN_GROUP_GET =
   "SELECT phone_number FROM contacts WHERE id IN (SELECT contact_id FROM contacts_groups WHERE group_id = ? AND enabled = 1) AND enabled = 1";
